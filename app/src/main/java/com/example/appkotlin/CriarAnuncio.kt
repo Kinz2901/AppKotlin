36b4b1594414ca3eb41a2f44e2f59337ee954dac
Guilherme Lins <guilhermelins978@gmail.com>
2024-10-12 17:31:05 -0300

    criei a pagina Criar Anuncio

--- app/src/main/java/com/example/appkotlin/CriarAnuncio.kt
+++ app/src/main/java/com/example/appkotlin/CriarAnuncio.kt
@@ -4,18 +4,19 @@ package com.example.appkotlin
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 
 class CriarAnuncio : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.criar_anuncio)
 
 
-        val buttonProduct = findViewById<Button>(R.id.buttonProduct)
+        val setaBack = findViewById<ImageView>(R.id.setaBack)
 
-        buttonProduct.setOnClickListener {
+        setaBack.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
