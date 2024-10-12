8647b42dcc0a85eca6a63c8eebf04768e36e6c47
Guilherme Lins <guilhermelins978@gmail.com>
2024-10-12 16:31:21 -0300

    Criei 2 telas / ta funcionando

--- app/src/main/java/com/example/appkotlin/CriarAnuncio.kt
+++ app/src/main/java/com/example/appkotlin/CriarAnuncio.kt
@@ -1,11 +1,13 @@
 package com.example.appkotlin
 
+
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity2 : AppCompatActivity() {
+
+class CriarAnuncio : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
