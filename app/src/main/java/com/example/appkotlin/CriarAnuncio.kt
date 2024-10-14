93370195e1b94b6d500701a3927f17b19fe79f0f
Guilherme Lins <guilhermelins978@gmail.com>
2024-10-14 19:36:47 -0300

    Quase finalizei a tela de criar produtos
    Quase finalizei a tela de criar produtos

--- app/src/main/java/com/example/appkotlin/CriarAnuncio.kt
+++ app/src/main/java/com/example/appkotlin/CriarAnuncio.kt
@@ -4,9 +4,12 @@ package com.example.appkotlin
 import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils.replace
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.startActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ReportFragment.Companion.reportFragment
 
@@ -16,8 +19,15 @@ class CriarAnuncio : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.criar_anuncio)
 
-
         val setaBack = findViewById<ImageView>(R.id.setaBack)
+
+        val categorias = arrayOf("Esportes", "Games", "Eletronicos", "Roupas", "Brinquedos", "Outros")
+
+        val autoCompleteTxt: AutoCompleteTextView = findViewById(R.id.autoCompleteTxt)
+
+        val adapterItems = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, categorias)
+        autoCompleteTxt.setAdapter(adapterItems)
+
 
         setaBack.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
