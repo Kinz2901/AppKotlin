eea6270152b7104d0f3c1df4f9b78a379e0839ac
Guilherme Lins <guilhermelins978@gmail.com>
2024-10-25 18:25:38 -0300

    Teste

--- app/src/main/java/com/example/appkotlin/HomeFragment.kt
+++ app/src/main/java/com/example/appkotlin/HomeFragment.kt
@@ -5,25 +5,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import com.example.appkotlin.model.Produto
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
+    private lateinit var nameNewProductTextView: TextView
+    private val produtos: MutableList<Produto> = mutableListOf()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val produto = Produto(
+            imagem = "https://example.com/imagem.jpg",
+            nome = "Produto Exemplo",
+            descricao = "Este é um exemplo de descrição do produto.",
+            preco = 49.99,
+            quantidade = 10,
+            categoria = "Eletrônicos"
+        )
+        produtos.add(produto)
+        return view
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        nameNewProductTextView = view.findViewById(R.id.nameNewProduct)
+    }
+    fun updateText(newNome: String) {
+        println(newNome)
     }
 
 }
