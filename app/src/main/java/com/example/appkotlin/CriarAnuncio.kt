8e22651c1384f9708d3c2c2b7e86ff2ef03152d2
Guilherme Lins <guilhermelins978@gmail.com>
2024-11-11 15:06:01 -0300

    Consegui adicionar a url no banco de dados

--- app/src/main/java/com/example/appkotlin/CriarAnuncio.kt
+++ app/src/main/java/com/example/appkotlin/CriarAnuncio.kt
@@ -15,6 +15,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import java.util.UUID
 
 class CriarAnuncio : AppCompatActivity() {
 
@@ -22,9 +24,13 @@ class CriarAnuncio : AppCompatActivity() {
         FirebaseFirestore.getInstance()
     }
 
-    var imageUri: Uri? = null
+    private val armazenamento by lazy {
+        FirebaseStorage.getInstance()
+    }
 
-    private val pickerMedia = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+    private var imageUri: Uri? = null
+
+    private val pickerMedia = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
         uri?.let {
             imageUri = uri
             val imgView = findViewById<ImageView>(R.id.imgView)
@@ -56,15 +62,12 @@ class CriarAnuncio : AppCompatActivity() {
             val quantidade = quantProduct.text.toString().toInt()
             val categoria = autoCompleteText.text.toString()
 
-            println(imageUri.toString())
-
-            salvarDados(nome, descricao, preco, quantidade, categoria)
-
+            uploadGaleria(nome, descricao, preco, quantidade, categoria)
         }
 
 
         imgView.setOnClickListener {
-            pickerMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+            pickerMedia.launch("image/*")
         }
 
         setaBack.setOnClickListener {
@@ -72,9 +75,30 @@ class CriarAnuncio : AppCompatActivity() {
         }
     }
 
-    private fun salvarDados(nome: String, descricao: String, preco: Double, quantidade: Int, categoria: String) {
+
+    val nomeImagem = UUID.randomUUID().toString()
+    private fun uploadGaleria(nome: String, descricao: String, preco: Double, quantidade: Int, categoria: String) {
+        if ( imageUri != null ) {
+            armazenamento
+                .getReference("produtos")
+                .child(nomeImagem)
+                .putFile( imageUri!! )
+                .addOnSuccessListener { task ->
+                    exibirMensagem("Sucesso ao fazer upload da imagem")
+                    task.metadata?.reference?.downloadUrl
+                        ?.addOnSuccessListener { uriFirebase ->
+                            salvarDados(uriFirebase.toString(), nome, descricao, preco, quantidade, categoria)
+                        }
+                }.addOnFailureListener{ erro ->
+                    exibirMensagem("Erro ao fazer upload da imagem")
+                }
+        }
+    }
+
+    private fun salvarDados(imagemUrl: String, nome: String, descricao: String, preco: Double, quantidade: Int, categoria: String) {
 
         val produto = mapOf(
+            "imagemUrl" to imagemUrl,
             "nome" to nome,
             "preco" to preco,
             "descricao" to descricao,
